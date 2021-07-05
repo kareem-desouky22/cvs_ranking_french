86b48da9e88c5c9f0f348ea62265b1c156ae779f
Sajid Ur Rehman <34974698+ExpertsVision@users.noreply.github.com>
2021-07-05 18:24:17 +0500

    removed unnecessary comments

--- src/features_extraction_1.py
+++ src/features_extraction_1.py
@@ -67,13 +67,11 @@ def convert_to_paras(filename):
         
        
 def extract_qualifications(i,sentence,qualifications_list,cv_name):
-#    lsm_q = spacy.load(r"C:\Projects\CVsRnkingAmir\code with dataset\code with dataset\model\Qualification_model_fre")
     doc_qual=lsm_q(sentence)
     for X in doc_qual.ents:
         if X.label_=="qualification":
             contains_digit = any(map(str.isdigit, X.text))
             if contains_digit or ':' in X.text:
-#                print ('it contains numeric value')
                 pass
             else:            
                 qualifications_list.append(X.text)
@@ -99,16 +97,12 @@ def extract_locations(i,sentence,locations_list,cv_name):
     doc_all=nlp(sentence)
     for X in doc_all.ents:
         if (X.label_=='GPE'):
-            locations_list.append(X.text)
-            
+            locations_list.append(X.text)            
     words_list=sentence.split(' ')
-
     for w in words_list:
         if w in l_list:
-            # print ('found location')
             locations_list.append(w)
     loc_list=top_frequent(locations_list, k=3)
-#    print ('length of locations list is:',len(loc_list))
     return(loc_list)
             
 def extract_languages(i,sentence,languages_list,cv_name):
@@ -117,123 +111,64 @@ def extract_languages(i,sentence,languages_list,cv_name):
     for X in doc_all.ents:
         if (X.label_=='LANGUAGE'):
             languages_list.append(X.text)
-            
-            
     words_list=sentence.split(' ')
-
     for w in words_list:
         if w in lang_list:
-            # print ('found language')
             languages_list.append(w)
-            
-
-#    if (len_languages)<=5:
-#        pass
-#    else:
-#    print ('there are', len_languages, 'in languages list')
     lang_list=top_frequent(languages_list, k=3)
-#    print ('length of languages list is:',len(lang_list))
     return(lang_list)
 
 
-def extract_phone(i,sentence,phone_list,cv_name):
-    
+def extract_phone(i,sentence,phone_list,cv_name):    
     phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-
     mo = phoneNumRegex.search(sentence)
     if mo  is not None:
         phone_list.append(mo.group())
-    
-    
 
-    
-def extract_email(i,sentence,email_list,cv_name):
-    
+def extract_email(i,sentence,email_list,cv_name):    
     s = re.sub(r"\s+"," ", sentence, flags = re.I)
-
     email_list.append(re.findall('\S+@\S+', s))
-    
-    
 
-#    return(email_list)
 
 def make_couples(job_title_list,exp_ind,dates_index):
-    # print('MAKING COUPLES...')
-    # print ('exp index is:', exp_ind)
-    # print ('dates-index is:', dates_index)
-    
-    # print ('exp index is:', exp_ind)
-    # print ('dates index is:', dates_index)
     thresh= 4
     couples= []
     matched_dates=[]
     for i in exp_ind:
-        
-        # print('exp_ind', i)
         if len(dates_index)>0:
-        
             closest_match= min(dates_index, key=lambda x:abs(x-i))
-            # print ('closest_match' , closest_match)
             difference_inds= (i-closest_match)
-            # print('difference_inds', difference_inds)
             if difference_inds>=0 and difference_inds<=thresh and closest_match not in matched_dates:
-                # couples.append([i,closest_match,'end date'])
                 couples.append([i,closest_match, 'start_date', 'end_date',30])
                 matched_dates.append(closest_match)
-                # print ('closest match is negative')
-                
+              
             else:
                 thresh= -4
                 if  difference_inds>=thresh and closest_match not in matched_dates:
-                    # print ('difference is less than threshold value')
-                # couples.append([i,closest_match,'end date'])
                     couples.append([i,closest_match,'start_date', 'end_date',30])
                     matched_dates.append(closest_match)
-                
-                
-            # print ('there is no date present')
-            # couples.append([i,'no date'])
-            # matched_dates.append('no date')
         else:
-            
-#            print ('there is no date present')
             couples.append([i,'no date', 'start_date', 'end_date',30])  # 30 days of duration by default
             matched_dates.append('no date')
-            
-    # print('matched all couples')
-    # print ('**************************************************************')
+
     return couples
         
   
 def extract_job_date(i,job_title_list,sentence,experience_list,experience_index,dates_index,cv_name):
-#    lsm_j=spacy.load(os.path.join (models_folder,'job_model_fre'))
     doc_job=lsm_j(sentence)
-            # print('job#########',sentence,'same')
     for X in doc_job.ents:
         if X.label_=='job title':
-                    
-#                     print('job*******************',X.text)
             job_found=X.text
-            # print ('Job found and value is:',job_found)
             experience_list.append(job_found)
             experience_index.append(i)
             job_title_list.append(job_found)
-            # print ('experience found is:', job_found)
-                    
+
     date_=search_dates(sentence,languages=['fr']) 
     if str(type(date_))!="<class 'NoneType'>":
-        # print ('date found')
-        # print ('date is:', date_)
         dates_index.append(i)
         
-        
-    
-    
 
 def extract_all_features(all_paras,cv_name):
-    # print ('=============================================================')
- 
-    count=0
     job_found=''
     job_title_list=[]
     dates_list=[]
@@ -248,59 +183,25 @@ def extract_all_features(all_paras,cv_name):
     email_list=[]
     phone_list=[]
     all_text=all_paras.paragraphs
-    
-    
-    
-    
-    # print ('length of all para is:', len(all_text))
-    for i,para in enumerate(all_text):
-        # print ('value of i is:',i)
-        temp = []
 
+    for i,para in enumerate(all_text):
         sentence=para.text.lower().strip()
-        
-            # if sentence!="":
-        sentence=sentence.strip().lower()
-        # sentence = re.sub(r'[.|,|:|\']',' ', sentence) #filter out all the commas, periods, and appostrophes using rege
-        # sentence=re.sub('[^A-Za-z0-9À-ÿ]+', ' ', sentence)
-        # sentence= sentence.decode(encoding)
-        # sentence=re.sub("[\u0300-\u036f]", "", sentence)
+        sentence=sentence.strip()
         sentence=unidecode.unidecode(sentence)
         
         #remove multiple spaces
         sentence = re.sub(r"\s+"," ", sentence, flags = re.I)
-        
-        #remove spaces from start and end
         sentence = re.sub(r"^\s+", "", sentence)
-        
-        # remove single characters 
         sentence = re.sub(r"^\s+", "", sentence)
         all_sentences.append(sentence)
-            
-            # print('*****************',sentence)
-        
         extract_job_date(i,job_title_list,sentence,experience_list,experience_index,dates_index,cv_name)
-        # print ('job and date extracted')
         q_list=extract_qualifications(i,sentence,qualifications_list,cv_name)
-        # print ('qualification extracted')
         s_list=extract_skills(i,sentence,skills_list,cv_name)
-        # print ('skills extracted')
-        
         lang_list=extract_languages(i,sentence,languages_list,cv_name)  #language list
-        # print ('language extracted')
         loc_list=extract_locations(i,sentence,locations_list,cv_name)  #location list
-        # print ('locations extracted')
         extract_phone(i,sentence,phone_list,cv_name)  #location list
         extract_email(i,sentence,email_list,cv_name)  #location list
-        
-        
     couples= make_couples(job_title_list,experience_index,dates_index)
-    
-                      
-      
-        
-    # couples= make_couples(experience_index,dates_index)
-    # print(couples)
     return couples, experience_list, experience_index,dates_index,all_sentences, q_list, s_list, loc_list, lang_list, email_list, phone_list
                         
 
