600afb6552d727a05492da822cb7a6836061fcbf
Sajid Ur Rehman <34974698+ExpertsVision@users.noreply.github.com>
2021-08-13 17:39:49 +0500

    extracted and combined languages and locations

--- src/read_extract_jds.py
+++ src/read_extract_jds.py
@@ -35,7 +35,7 @@ def extract_skills(clean_sent):
                 pass
             else:
                 skills_list.append(X.text)
-#    skills_list=top_frequent(skills_list, k=15)
+#    skills_list=top_frequent(skills_list, k=8)
     return(skills_list)
 
 def extract_qualifications(clean_sent):
@@ -49,8 +49,35 @@ def extract_qualifications(clean_sent):
             else:            
                 qualifications_list.append(X.text)
                 
+#    qualifications_list=top_frequent(qualifications_list, k=8)             
     return qualifications_list
                 
+            
+def extract_locations(sent):
+    locations_list=[]
+    doc_all=nlp(sent)
+    for X in doc_all.ents:
+        if (X.label_=='GPE'):
+            locations_list.append(X.text)            
+    words_list=sent.split(' ')
+    for w in words_list:
+        if w in l_list:
+            locations_list.append(w)
+#    loc_list=top_frequent(locations_list, k=3)
+    return(locations_list)
+            
+def extract_languages(sent):
+    languages_list=[]
+    doc_all=nlp(sent)
+    for X in doc_all.ents:
+        if (X.label_=='LANGUAGE'):
+            languages_list.append(X.text)
+    words_list=sent.split(' ')
+    for w in words_list:
+        if w in lang_list:
+            languages_list.append(w)
+#    lang_list=top_frequent(languages_list, k=3)
+    return(lang_list)
 
 
 
@@ -58,6 +85,8 @@ def extract_qualifications(clean_sent):
 
 data_path=os.path.join(os.path.abspath(os.path.join(__file__,"../../")),'data')
 models_folder=os.path.join(data_path,'models')
+location_txt=models_folder+'\locations.txt'
+language_txt=models_folder+'\languages.txt'
 results_folder=os.path.join(data_path,'results')
 
 
@@ -67,8 +96,18 @@ nlp = spacy.load ('fr_core_news_lg')
 lsm_j=spacy.load(os.path.join (models_folder,'job_model_fre'))
 lsm_s=spacy.load(os.path.join (models_folder,'skill_model_fre'))
 lsm_q=spacy.load(os.path.join (models_folder,'Qualification_model_fre'))
-location_txt=models_folder+'\locations.txt'
-language_txt=models_folder+'\languages.txt'
+
+
+with open (location_txt, "r",encoding="utf8") as myfile:
+    data=myfile.read()
+l_list=data.split ('\n')
+l_list=[l.lower() for l in l_list if l!=""]     
+
+with open (language_txt, "r",encoding="utf8") as myfile:
+    data=myfile.read()                
+lang_list=data.split ('\n')
+lang_list=[lang.lower() for lang in lang_list] 
+
 
 #json_file=os.path.join(results_folder,'jds_5.json')
 #
@@ -93,6 +132,7 @@ df_jd['candidate_profile'] = df_jd.apply(lambda x: clean_string(x['Candidate-pro
 
 df_jd['job_mission'] = df_jd.apply(lambda x: clean_string(x['Job_mission']), axis = 1)
 
+df_jd['job_misc'] = df_jd.apply(lambda x: clean_string(x['Job-miscs']), axis = 1)
 
 # Extraction of skills and qualifications from Candidate profile:
 
@@ -110,6 +150,11 @@ df_jd['skills_list_mission'] = df_jd.apply(lambda x: extract_skills(x['job_missi
 df_jd['qualifications_list_mission'] = df_jd.apply(lambda x: extract_qualifications(x['job_mission']), axis = 1)
 
 
+# Extraction of languages and locations from Job Misc:
+df_jd['final_langs_jd'] = df_jd.apply(lambda x: extract_locations(x['job_misc']), axis = 1)
+
+df_jd['locations_list_misc'] = df_jd.apply(lambda x: extract_languages(x['job_misc']), axis = 1)
+
 #  Final qualification list
 df_jd['final_qualifications_jd'] =df_jd['qualifications_list_profile']+df_jd['qualifications_list_mission']+df_jd['qualifications_jd']
 
@@ -117,8 +162,12 @@ df_jd['final_qualifications_jd'] =df_jd['qualifications_list_profile']+df_jd['qu
 #  Final skills list
 df_jd['final_skills_jd'] =df_jd['skills_list_profile']+df_jd['skills_list_mission']+df_jd['skills']
 
+#  Final locations list
+df_jd['final_locs_jd'] =df_jd['locations_list_misc']+df_jd['location_jd']
 
-df_jd.drop(['Candidate-profile','Job_mission','candidate_profile','job_mission','skills_list_profile','skills_list_mission',],axis=1, inplace=True)
+
+# Delete unnecessary columns
+df_jd.drop(['Candidate-profile','Job_mission','candidate_profile','job_mission','skills_list_profile','skills_list_mission','Job-miscs','Job_introduction','industry','Sub-industry','tags','roleDesignation','skills','jobDescription','qualifications_jd','location_jd','qualifications_list_profile','qualifications_list_mission','locations_list_misc'],axis=1, inplace=True)
 
 #df_jd.to_csv('jds.csv')
 
